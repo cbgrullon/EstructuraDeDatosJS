48187d70b99e7ceaad0cca5b601152640cf796d0
cgrullon <christopher.gru@gmail.com>
2019-05-08 21:49:51 -0400

    Adding Timmer

--- selectionOrder.js
+++ selectionOrder.js
@@ -1,4 +1,5 @@
 function SelectionOrder(array){
+    let hourStart= process.hrtime();
     let outputArray=[];
     let arr = Array.from(array);
     for(var index =0; index<array.length;index++){
@@ -6,7 +7,9 @@ function SelectionOrder(array){
         outputArray.push(result.menor);
         arr.splice(result.menorIndex,1);
     }
-    return outputArray;
+    console.log(outputArray);
+    let hourEnd = process.hrtime(hourStart);
+    console.info('Execution time (hr): %ds %dms', hourEnd[0], hourEnd[1] / 1000000)
 }
 function getMenor(array){
     let menor=array[0];
@@ -19,4 +22,4 @@ function getMenor(array){
     }
     return {menor,menorIndex};
 }
-console.log(SelectionOrder([5,84,95,6,1232312312,12313123,12312,12312,1231,345,53,54654,345345,23442,1231545,324241,9945]))
+SelectionOrder([5,84,95,6,1232312312,12313123,12312,12312,1231,345,53,54654,345345,23442,1231545,324241,9945])
