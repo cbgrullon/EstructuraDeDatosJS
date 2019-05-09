c06744eec1e91e446e4135fffc30b61c0a59c86d
cgrullon <christopher.gru@gmail.com>
2019-05-08 21:59:24 -0400

    v2

--- selectionOrder.js
+++ selectionOrder.js
@@ -1,25 +1,24 @@
 function SelectionOrder(array){
-    let hourStart= process.hrtime();
+    if(!array){
+        return;
+    }
     let outputArray=[];
-    let arr = Array.from(array);
-    for(var index =0; index<array.length;index++){
-        let result = getMenor(arr);
-        outputArray.push(result.menor);
-        arr.splice(result.menorIndex,1);
+    let start = process.hrtime();
+    while(1===1){
+        if(array.length===0){
+            break;
+        }
+        let menorIndex= 0;
+        for(var index=0;index<array.length;index++){
+            if(array[index]<array[menorIndex]){
+                menorIndex=index;
+            }
+        }
+        outputArray.push(array[menorIndex]);
+        array.splice(menorIndex,1);
     }
     console.log(outputArray);
-    let hourEnd = process.hrtime(hourStart);
-    console.info('Execution time (hr): %ds %dms', hourEnd[0], hourEnd[1] / 1000000)
-}
-function getMenor(array){
-    let menor=array[0];
-    let menorIndex= 0;
-    for(var index=0;index<array.length;index++){
-        if(array[index]<menor){
-            menor= array[index];
-            menorIndex=index;
-        }
-    }
-    return {menor,menorIndex};
+    let end = process.hrtime(start);
+    console.info('Execution time (hr): %ds %dms', end[0], end[1] / 1000000)
 }
 SelectionOrder([5,84,95,6,1232312312,12313123,12312,12312,1231,345,53,54654,345345,23442,1231545,324241,9945])
